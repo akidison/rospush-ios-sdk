b6a4bba23eada11bf813dc991ccc7bc4219e79fa
André Kis <akidis.developer@gmail.com>
2024-12-04 12:24:16 +0100

    init

---
+++ RosPush/RosPush.h
@@ -0,0 +1,18 @@
+//
+//  RosPush.h
+//  RosPush
+//
+//  Created by André Kis on 04.12.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RosPush.
+FOUNDATION_EXPORT double RosPushVersionNumber;
+
+//! Project version string for RosPush.
+FOUNDATION_EXPORT const unsigned char RosPushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RosPush/PublicHeader.h>
+
+
---
+++ RosPush/RosPushMetric.swift
@@ -0,0 +1,15 @@
+//
+//  RosPushMetric.swift
+//  RosPush
+//
+//  Created by André Kis on 04.12.24.
+//
+
+import Foundation
+
+public class RosPushMetric: NSObject {
+    
+    public static func configure(with appKey: String) {
+        
+    }
+}
